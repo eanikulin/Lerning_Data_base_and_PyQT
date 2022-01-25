557bca89b2ccfef711c52a990d1e5481a28efe67
eanikulin <ea.nikulin@yandex.ru>
2022-01-25 16:17:22 +0500

    //tasks 1,2,3 complete

--- Lesson_1/task_1_1.py
+++ Lesson_1/task_1_1.py
@@ -1,1 +1,23 @@
+import ipaddress
+import subprocess
 
+iplist = [ipaddress.ip_address('192.168.25.100'), ipaddress.ip_address('192.168.25.101'), 'yandex.ru']
+
+
+def host_ping(hosts):
+    res = dict()
+    for addr in hosts:
+        args = ['ping', str(addr)]
+        subproc_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
+        m_list = []
+        for line in subproc_ping.stdout:
+            line = line.decode('cp866').encode('utf-8').decode('utf-8')
+            m_list.append(line)
+        if 'TTL' in m_list[3]:
+            res[str(addr)] = 'Доступен'
+        else:
+            res[str(addr)] = 'Недоступен'
+    return res
+
+
+print(host_ping(iplist))
